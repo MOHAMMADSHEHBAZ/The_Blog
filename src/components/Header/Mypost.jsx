bfc764f1ddf4bdf50ec65bf6e994557b914cb1c5
shehbaz <shhbzsddq1@gmail.com>
2024-11-23 22:22:16 +0530

    commit

--- src/components/Header/Mypost.jsx
+++ src/components/Header/Mypost.jsx
@@ -1,28 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import service from '../../appwrite/auth/config';
-import { PostCard, Container } from '../index';
-import toast, { Toaster } from 'react-hot-toast';
-import authService from '../../appwrite/auth/auth';
-import { Query } from 'appwrite';
+import React, { useState, useEffect } from "react";
+import service from "../../appwrite/auth/config";
+import { PostCard, Container } from "../index";
+import toast, { Toaster } from "react-hot-toast";
+import authService from "../../appwrite/auth/auth";
+import { Query } from "appwrite";
+import { ThreeDots } from "react-loader-spinner";
 
 const MyPost = () => {
-
   const [posts, setPost] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMyPosts = async () => {
       try {
-        const user = await authService.getCurrentUser(); 
+        const user = await authService.getCurrentUser();
         const userId = user.$id;
-        const userPosts = await service.getPosts1([Query.equal('userId', userId)]);
+        const userPosts = await service.getPosts1([
+          Query.equal("userId", userId),
+        ]);
         if (userPosts) {
           setPost(userPosts.documents);
         } else {
-          toast.error('Failed to load posts.');
+          toast.error("Failed to load posts.");
         }
       } catch (error) {
-        toast.error('Error fetching posts');
+        toast.error("Error fetching posts");
       } finally {
         setLoading(false);
       }
@@ -32,16 +34,27 @@ const MyPost = () => {
   }, []);
 
   return (
-    <div className='w-full py-8 mt-20'>
+    <div className="w-full py-8 mt-20">
       <Toaster />
       <Container>
         {loading ? (
-          <p className='text-center  text-xl'>Loading...</p>
+          <p className="flex items-center justify-center text-center text-xl">
+          <ThreeDots
+            visible={true}
+            height="80"
+            width="80"
+            color="#"
+            radius="9"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </p>
         ) : (
-          <div className='flex flex-wrap'>
+          <div className="flex flex-wrap">
             {posts.length > 0 ? (
               posts.map((post) => (
-                <div key={post.$id} className='p-2 w-1/4'>
+                <div key={post.$id} className="p-2 w-1/4">
                   <PostCard {...post} />
                 </div>
               ))
--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,63 +1,76 @@
-import React from 'react'
-import service from '../appwrite/auth/config'
-import { PostCard,Container } from '../components/index'
-import { useState,useEffect } from 'react'
-import toast, { Toaster } from 'react-hot-toast'
-import authService from '../appwrite/auth/auth'
+import React from "react";
+import service from "../appwrite/auth/config";
+import { PostCard, Container } from "../components/index";
+import { useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import authService from "../appwrite/auth/auth";
+import { ThreeDots } from "react-loader-spinner";
 
 const AllPost = () => {
+  const [posts, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [name, setName] = useState("");
+  const [label, setLabel] = useState("");
 
-    const [posts,setPost] = useState([])
-    const [loading, setLoading] = useState(true);
-    const [name, setName] = useState('');
-    const [label, setLabel] = useState('');
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const user = await authService.getCurrentUser();
+        setName(user.name);
+        setLabel(user.labels);
+      } catch {
+        toast.error(error.message);
+        throw error;
+      }
+    };
+    fetchUsers();
+  }, []);
 
-    useEffect(()=>{
-        const fetchUsers = async () => {
-            try {
-                const user = await authService.getCurrentUser();
-                setName(user.name);
-                setLabel(user.labels);
-            }
-            catch{
-                toast.error(error.message);
-                throw error;
-            }
-        }
-        fetchUsers();
-    },[])
+  useEffect(() => {}, []);
+  service.getPosts([]).then((posts) => {
+    if (posts) {
+      setPost(posts.documents);
+      setLoading(false);
+    }
+  });
 
-    useEffect(() => {}, [])
-    service.getPosts([]).then((posts) => {
-        if (posts) {
-            setPost(posts.documents);
-            setLoading(false);
-        }
-    })
-    
-      
-    return (
-      <div className='w-full py-8 mt-20'>
-        <Toaster/>
-        {loading?(<>
-        </>):(
-            <p className='text-center top-0 text-2xl font-mono'>All Posts
-            {label=='admin' &&(<>(Admin)</>)}
-            </p>
+  return (
+    <div className="w-full py-8 mt-20">
+      <Toaster />
+      {loading ? (
+        <></>
+      ) : (
+        <p className="text-center top-0 text-2xl font-mono">
+          All Posts
+          {label == "admin" && <>(Admin)</>}
+        </p>
+      )}
+      <Container>
+        {loading ? (
+          <p className="flex items-center justify-center text-center text-xl">
+            <ThreeDots
+              visible={true}
+              height="80"
+              width="80"
+              color="#"
+              radius="9"
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </p>
+        ) : (
+          <div className="lg:flex flex-wrap mt-5">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-1/4">
+                <PostCard {...post} />
+              </div>
+            ))}
+          </div>
         )}
-          <Container>
-            {loading? ( <p className='text-center text-xl'>Loading...</p> ): (
-                <div className='flex flex-wrap mt-5'>
-                {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard {...post} />
-                    </div>
-                ))}
-            </div>
-            )}
-              </Container>
-      </div>
-    )
-  }
+      </Container>
+    </div>
+  );
+};
 
 export default AllPost;
